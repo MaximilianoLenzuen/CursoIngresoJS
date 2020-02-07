9c052086289b9aca67b0d34e12b907997b016bdc
alumno <alumno>
2020-02-07 12:20:13 -0300

    Segunda Clase

--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -1,6 +1,12 @@
 function mostrar()
 {
 //tomo la edad  
+var edad;
+edad = parseInt(document.getElementById("edad").value);
+if (edad == 15){
+    alert("Niña bonita");
+} 
+    
 
 
 
--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -1,7 +1,12 @@
 function mostrar()
 {
 //tomo la edad  
+var edad;
+edad = parseInt(document.getElementById("edad").value);
 
+if ( 13 <=  edad  ){
+    alert("Usted es adolescente");
+}
 
 
 }//FIN DE LA FUNCIÓN
