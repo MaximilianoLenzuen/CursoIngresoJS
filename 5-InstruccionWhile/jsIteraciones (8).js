c369958ee9d407eb07287fa65311fd27320ce618
MaximilianoLenzuen <60608482+MaximilianoLenzuen@users.noreply.github.com>
2020-02-21 12:19:21 -0300

    Add files via upload
    sexta clase

--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -4,9 +4,18 @@ function mostrar()
 	var contador=0;
 	var positivo=0;
 	var negativo=1;
-	
 	var respuesta='si';
 
+	do {
+		numero = parseInt(prompt("Ingrese un numero: "));
+		while (isNaN(numero)) {
+			numero = parseInt(prompt("Eso no es un numero, ingrese uno"));
+		}
+
+		acumulador = acumulador + numero;
+		contador = contador + 1;
+		seguir = prompt("quiere ingresar otro numero?");
+	} while (seguir == 's');
 
 document.getElementById('suma').value=positivo;
 document.getElementById('producto').value=negativo;
--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -2,17 +2,30 @@ function mostrar()
 {
 
 	var contador=0;
-	// declarar variables
-	
-	var respuesta='si';
+	var numero;
+	var maximo;
+	var mnimo;
+	var respuesta = 's' ;
 
-	while(respuesta!='no')
-	{
-		
-	
-	}
+	do {
+		numero = parseInt(prompt("Ingrese un numero: "));
+		while (isNaN(numero)) {
+			numero = parseInt(prompt("Eso no es un numero, ingrese uno"));
+		}
 
+		if (contador == 0 || numero > maximo){
+			maximo = numero;
+		}
+		if (contador == 0 || numero < maximo){
+			minimo = numero;
+		}
 
+		contador++;
 
+		respuesta = prompt("quiere ingresar otro numero?");
+	} while (respuesta == 's');
+
+	document.getElementById("maximo").value = maximo;
+	document.getElementById("minimo").value = minimo;
 
 }//FIN DE LA FUNCIÓN
