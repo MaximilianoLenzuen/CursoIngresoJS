cebab0d5b2a42d74a908722e468437486a602a9c
MaximilianoLenzuen <60608482+MaximilianoLenzuen@users.noreply.github.com>
2020-03-01 21:33:14 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,4 +1,66 @@
 function mostrar()
 {
+    var letra;
+    var numero;
+    var respuesta="si";
+    var numeroPar=0;
+    var numeroImpar=0;
+    var numeroCero=0;
+    var promedioPositivos=0;
+    var sumaNegativos=0;
+    var numeroMaximo=-100;
+    var letraMaximo;
+    var numeroMinimo=100;
+    var letraMaximo;
 
-}
+    while(respuesta=="si"){
+        letra = prompt("Ingrese una letra: ");
+        while(!(isNaN(letra))){
+            letra = prompt("Ingrese una letra valida!!!");
+        }
+
+        numero = parseInt(prompt("Ingrese un numero"));
+        while(numero > 100 || numero < -100){
+            numero = parseInt(prompt("Ingrese un numero valido!"));
+        }
+        
+
+        if(numero%2 == 0){
+            numeroPar++;
+        }else{
+            numeroImpar++;
+        }
+        if(numero==-100 || numero ==100){
+            numeroCero=numeroCero+2;
+        }else {
+            if(numero%10==0){
+                numeroCero++;
+            }
+        }
+        
+        if(numero>0){
+            promedioPositivos = promedioPositivos + numero;
+        }else{
+            sumaNegativos = sumaNegativos + numero;
+        }
+        if(numero<numeroMaximo){
+            numeroMaximo = numero;
+            letraMaximo = letra;
+        }
+        if(numero>numeroMinimo){
+            numeroMinimo=numero;
+            letraMinimo= letra;
+        }
+        
+    
+        respuesta = prompt("Desea ingresar otro numero?  si/no");
+    }
+
+    document.write("La cantidad de numeros pares son: " + numeroPar + "</br>" + "La cantidad de impares son: " + numeroImpar + "</br>" + "La cantidad de ceros son: " + numeroCero + "</br>" );
+    }
+
+    
+        
+    
+    
+
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,46 @@
 function mostrar()
 {
+    var nota;
+    var sexo;
+    var acumulador=0;
+    var contador=0;
+    var notaMinima=10;
+    var sexoMinimo;
+    var contadorMasculino=0;
+
+    while(contador<5){
+        
+        nota=parseInt(prompt("ingrese la nota"));
+            while (nota<1 || nota>10) {
+                nota=parseInt(prompt("Reingrese la nota"));
+            }
+
+            
+
+        sexo=prompt("identifique el sexo f o m");
+            while (sexo!='f'&& sexo!='m') {
+                sexo=prompt("identifique el sexo f o m");
+            }
+
+
+
+
+
+
+            if(nota<notaMinima){
+                notaMinima = nota;
+                sexoMinimo = sexo;
+            }
+            
+            if(sexo =="m" && nota >= 6){
+                contadorMasculino++;
+            }
+        
+            
+        contador++;
+        acumulador=acumulador+nota;
+    }
+    alert("el promedio es: "+ acumulador/contador);
+    alert("La cantidad de hombres con nota mayor a 6 es:" + contadorMasculino);
 
 }
